33baab30461dc3fab8cfc4709ac88eebb33f18c0
Poussou Elie <poussoueli@eisti.eu>
2016-06-12 14:55:07 +0200

    get neighbours for one cell

--- Board.js
+++ Board.js
@@ -1,6 +1,8 @@
 var Cell = require("./Cell")
 
 function Board(width, height){
+  this.width = width;
+  this.height = height;
   this.cells = [];
   for (var i = 0; i < width*height; i++) {
     this.cells[i] = new Cell(Math.random() < 0.5 ? true : false)
@@ -10,8 +12,47 @@ function Board(width, height){
 Board.prototype.getSize = function(){
   return this.cells.length;
 }
-Board.prototype.setNeighbours = function(cell){
-  return cell;
+
+Board.prototype.setNeighbours = function(cell, index){
+  var column = index % this.width
+  var row = Math.trunc(index / this.width)
+  var neighbours = [];
+  if(column == 0){
+    neighbours.push(this.cells[index+1]);
+  } else if(column == this.width - 1){
+    neighbours.push(this.cells[index-1]);
+  } else {
+    neighbours.push(this.cells[index-1], this.cells[index+1]);
+  }
+  if(row == 0){
+    if(column == 0){
+      neighbours.push(this.cells[index+this.width],
+         this.cells[index+this.width+1]);
+    } else if(column == this.width - 1){
+      neighbours.push(this.cells[index-1+this.width], this.cells[index+this.width]);
+    } else {
+      neighbours.push(this.cells[index-1+this.width], this.cells[index+this.width],
+         this.cells[index+this.width]+1);
+    }
+
+  } else if(row == this.height - 1){
+    if(column == 0){
+      neighbours.push(this.cells[index-this.width],
+        this.cells[index+1-this.width]);
+    }else if(column == this.width - 1){
+      neighbours.push(this.cells[index-1-this.width], this.cells[index-this.width]);
+    }else {
+      neighbours.push(this.cells[index-1-this.width], this.cells[index-this.width],
+        this.cells[index+1-this.width]);
+    }
+  } else {
+    neighbours.push(this.cells[index-1-this.width], this.cells[index-this.width],
+      this.cells[index+1-this.width]);
+    neighbours.push(this.cells[index-1+this.width], this.cells[index+this.width],
+       this.cells[index+this.width]);
+  }
+  var resCell = new Cell(cell.state, neighbours.filter((neighbour)=>neighbour));
+  return resCell;
 }
 
 
--- spec/BoardSpec.js
+++ spec/BoardSpec.js
@@ -1,10 +1,28 @@
 var Board = require("../Board")
 var Cell = require("../Cell")
 
-describe("A board has a constructor", function() {
+describe("A board", function() {
 
   it("should create instantiate a list of cells with the good length", function() {
     var board = new Board(2,3);
     expect(board.getSize()).toEqual(2*3);
+  });
+
+  it("should get at least 2 neighbours", function() {
+    var board = new Board(4,8)
+    var cell =  board.setNeighbours(board.cells[12], 12);
+    expect(cell.neighbours.length).toBeGreaterThan(1);
+  });
+  
+  it("should get 3 neighbours for a cell in boarder (first)", function() {
+    var board = new Board(4,8)
+    var cell =  board.setNeighbours(board.cells[0], 0);
+    expect(cell.neighbours.length).toEqual(3);
+  });
+
+  it("should get 3 neighbours for a cell in boarder (last)", function() {
+    var board = new Board(4,8)
+    var lastCell = board.setNeighbours(board.cells[board.getSize()-1], board.getSize()-1)
+    expect(lastCell.neighbours.length).toEqual(3);
   });
 });
